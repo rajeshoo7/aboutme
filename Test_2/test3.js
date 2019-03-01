a21d68506310d05248be68ade6beb48c6fcb201e
rajeshoo7 <rajeshsai.kammari@gmail.com>
2019-03-01 16:54:07 -0600

    Final Commit for p3

--- Test_2/test3.js
+++ Test_2/test3.js
@@ -5,7 +5,7 @@ assert.equal(myfunction(3),9,'Tested with two relatively small positive numbers'
 assert.equal(myfunction(0),0,'Tested with side value 0');
 assert.equal(myfunction(999887464544),0,'Tested with hours<0, result will be 0');
 assert.equal(myfunction(-9),0,'Tested with side value 0.')
-
+assert.equal(myfunction(-12),0,'Tested with side value 0.')
 
 
    // assert.ok( 4 == "input", "Passed!" );
